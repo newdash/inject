8b1fbc4df3514d3b16d6c2a122dba676d01fd0a7
Theo Sun <theo.sun@sap.com>
2020-08-19 15:28:58 +0800

    fix: type

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata";
-
 export * from './container';
 export * from './decorators';
 export * from './provider';
+export { InjectWrappedInstance, OptionalConstructorParameters, OptionalParameters, PromiseConstructor } from "./utils";
 
